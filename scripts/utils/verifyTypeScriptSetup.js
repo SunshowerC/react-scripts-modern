8693b9de17a28061efcee1bf427c28de76fa35a1
Weiyu Chen <weiyu.chen@shopee.com>
2018-12-13 14:24:00 +0800

    fix: update reference

--- scripts/utils/verifyTypeScriptSetup.js
+++ scripts/utils/verifyTypeScriptSetup.js
@@ -255,7 +255,7 @@ function verifyTypeScriptSetup() {
   if (!fs.existsSync(paths.appTypeDeclarations)) {
     fs.writeFileSync(
       paths.appTypeDeclarations,
-      `/// <reference types="react-scripts" />${os.EOL}`
+      `/// <reference types="react-scripts-modern" />${os.EOL}`
     );
   }
 }
